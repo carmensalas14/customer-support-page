1b309a3bfd2136a7be27f367b12f73a8d5e3f41f
Carmen Salas <carmensalas0114@gmail.com>
2020-08-24 07:34:15 -0400

    changed port

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const indexRouter = require('./routes/index');
 
 const app = express();
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.set('views', path.join(__dirname, 'views'));
 
